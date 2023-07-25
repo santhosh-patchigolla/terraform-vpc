83291d6f407a3e48a267ea888909407200c5c06d
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-25 12:37:28 +0530

    sdc

---
+++ terraform.auto.tfvars
@@ -0,0 +1,2 @@
+AZ      =  [ "us-east-1a","us-east-1b" ]
+
--- vars.tf
+++ vars.tf
@@ -1,4 +1,6 @@
 variable "VPC_CIDR" {}
 variable "ENV" {}
-
+variable "AZ" {}
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
 
